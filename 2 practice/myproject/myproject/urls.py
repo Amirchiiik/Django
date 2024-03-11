13ec223b2594170d1fe62ec288080d13ba3c0ea8
Amirkhan <98800860+Amirchiiik@users.noreply.github.com>
2024-03-11 21:22:46 +0600

    Delete 2 practice/myproject directory

--- 2 practice/myproject/myproject/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include  # Import include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('students.urls')),  # Include the students app URLs
-]
--- 2 practice/myproject/students/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def students_list(request):
-    students = ['Amirkhan', 'Serikbay', 'Arsen', 'Nusip']
-    return render(request, 'students/students_list.html', {'students': students})
